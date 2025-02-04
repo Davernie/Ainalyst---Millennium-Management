259946e82c41dedecf6268e5bbf7cf275215ad9b
Alex-Rybak <30793837+Alex-Rybak@users.noreply.github.com>
2025-02-04 15:51:21 +0000

    made changes providing support for earlier versions of Python

--- ast-parser/ast-parser/fileForTesting.py
+++ ast-parser/ast-parser/fileForTesting.py
@@ -1,6 +1,8 @@
 def partition_string(my_string):
     strings = [""]
     if False:
+        print("a")
+    while False:
         this_char=my_string[0]
         print(this_char)
         if this_char in strings[-1]:
@@ -9,4 +11,5 @@ def partition_string(my_string):
             strings[-1]+= this_char
             my_string=my_string[1:]
     print(strings)
+    #return strings
 
