13f63ffb3b2379e039f12e49e880c8f4f2c0b3a0
Naveen Senthil Kumar <senthiln@tcd.ie>
2025-02-11 14:45:15 -0800

    Add Code Smells Logic

--- backend/app/api/v1/endpoints/parse.py
+++ backend/app/api/v1/endpoints/parse.py
@@ -4,6 +4,8 @@ from pydantic import BaseModel
 from services.ast_parsing_service import astParser
 from services.liniting_service import run_pep8_linter
 
+from backend.app.services.code_smells_service import check_code_smell
+
 router = APIRouter()
 
 class CodeInput(BaseModel):
@@ -21,8 +23,11 @@ async def analyze_code(data: CodeInput):
     # Run PEP8 compliance check
     pep8_issues = run_pep8_linter(code)
 
+    code_smells = check_code_smell(code)
+
     return {
         "AST Issues": ast_issues if ast_issues else "No AST issues found.",
-        "PEP8 Issues": pep8_issues
+        "PEP8 Issues": pep8_issues,
+        "Code Smells": code_smells
     }
 
