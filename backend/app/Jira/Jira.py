d5cbaeeb72c56e391e1e635d430debc9f00ae0b7
eortiz <eortiz@tcd.ie>
2025-03-02 18:30:34 +0000

    updated jira file so that it is a function than can be used by other programs

--- backend/app/Jira/Jira.py
+++ backend/app/Jira/Jira.py
@@ -1,15 +1,24 @@
 import os
 from dotenv import load_dotenv
-from jira import JIRA\
+from jira import JIRA
 
 load_dotenv()
 
-jira_server = os.getenv('JIRA_SERVER')
-jira_email = os.getenv('JIRA_EMAIL')
-jira_API_token = os.getenv('JIRA_API_TOKEN')
-issue_key = os.getenv('BRANCH_NAME')
+def get_jira_description():
+    jira_server = os.getenv('JIRA_SERVER')
+    jira_email = os.getenv('JIRA_EMAIL')
+    jira_API_token = os.getenv('JIRA_API_TOKEN')
+    issue_key = os.getenv('BRANCH_NAME')
 
-jira = JIRA(server=jira_server, basic_auth=(jira_email, jira_API_token))
-issue = jira.issue(issue_key)
+    if not jira_server or not jira_email or not jira_API_token or not issue_key:
+        raise ValueError("One or more JIRA credentials are missing.")
 
-print(f"Issue Description: {issue.fields.description}")
+    jira = JIRA(server=jira_server, basic_auth=(jira_email, jira_API_token))
+    issue = jira.issue(issue_key)
+    description = issue.fields.description
+    print(f"Issue Description: {description}")
+    return description
+
+# Just used to test the function
+if __name__ == "__main__":
+    get_jira_description()
