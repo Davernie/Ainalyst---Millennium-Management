9786d9ef68826d8f98a18030263c0c7f034e1f9d
Alex-Rybak <30793837+Alex-Rybak@users.noreply.github.com>
2025-03-07 04:11:36 +0000

    finally frontend works

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -1,10 +1,9 @@
 # api/v1/__init__.py
 from fastapi import APIRouter
-from .endpoints import health,parse,returnlog
+from .endpoints import health,parse
 
 
 api_router = APIRouter()
 api_router.include_router(health.router)
 api_router.include_router(parse.router)
-api_router.include_router(returnlog.router)
 
