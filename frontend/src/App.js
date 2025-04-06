bc368f175493dc4f6ef7583e0128161c993c44b5
Alex-Rybak <30793837+Alex-Rybak@users.noreply.github.com>
2025-04-06 01:29:49 +0100

    jira login finally working as intended

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,55 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from "react-router-dom";
+import { JiraProvider, JiraContext } from "./JiraContext";
 import Home from "./pages/Home";
 import History from "./pages/History";
-import logo from "./logo.svg"; 
-import logo2 from "./AI-nalyst.svg"; 
+import JiraLogin from "./pages/JiraLogin";
+import logo from "./logo.svg";
+import logo2 from "./AI-nalyst.svg";
 import "./App.css";
+import { useContext } from "react";
+
+function NavBar() {
+  const { jiraUser } = useContext(JiraContext);
+  const navigate = useNavigate();
+
+  return (
+    <nav>
+      <div className="nav-container">
+        <div className="logo-group">
+          <img src={logo} alt="Logo" className="nav-logo small-logo" />
+          <img src={logo2} alt="AI-nalyst Logo" className="nav-logo big-logo" />
+        </div>
+
+        <ul>
+          <li><Link to="/home">Home</Link></li>
+          <li><Link to="/history">History</Link></li>
+        </ul>
+
+        {jiraUser && (
+          <div className="jira-user" onClick={() => navigate("/")}>
+            {jiraUser}
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+}
 
 function App() {
   return (
-    <Router>
-      <nav>
-        <div className="nav-container">
-        <div className="logo-group">
-            <img src={logo} alt="Logo" className="nav-logo small-logo" />
-            <img src={logo2} alt="AI-nalyst Logo" className="nav-logo big-logo" />
-          </div>
-
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/history">History</Link></li> 
-          </ul>
-          
+    <JiraProvider>
+      <Router>
+        <NavBar />
+        <div className="app">
+          <Routes>
+            <Route path="/" element={<JiraLogin />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/history" element={<History />} />
+          </Routes>
         </div>
-      </nav>
-
-      <div className="app">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/history" element={<History />} />
-        </Routes>
-      </div>
-      
-    </Router>
+      </Router>
+    </JiraProvider>
   );
 }
 
 export default App;
-
